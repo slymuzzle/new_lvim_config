d68338b919907a8cb72f85c1d463616ba130e0e8
Sly Muzzle <slymuzzle@gmail.com>
2022-09-20 18:46:21 +0600

    Fix configs

--- config.lua
+++ config.lua
@@ -137,6 +137,9 @@ lvim.builtin.nvimtree.setup.filters.exclude = { "node_modules" }
 lvim.builtin.lualine.options.globalstatus = true
 lvim.builtin.lualine.options.disabled_filetypes = {}
 
+-- Configure Breadcrumbs
+lvim.builtin.breadcrumbs.active = true
+
 -- Configure Bufferline
 lvim.builtin.bufferline.options.always_show_bufferline = true
 lvim.builtin.bufferline.options.offsets = {
@@ -319,7 +322,7 @@ lvim.plugins = {
                 use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
                 respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
                 cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-                easing_function = 'quadratic', -- Default easing function
+                easing_function = 'sine', -- Default easing function
                 pre_hook = nil, -- Function to run before the scrolling animation starts
                 post_hook = nil, -- Function to run after the scrolling animation ends
             })
